808e48747e1e1914eca8140f4bc446d89d4195c3
Prabin Bohara <prabinbohara10@gmail.com>
2022-06-20 14:58:00 +0545

    total_page find garne optimize vayo

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-#yo chai just total_page access garana. But, this can be optimize
-url= "https://bg.annapurnapost.com/api/search?title=%E0%A4%AC%E0%A4%9C%E0%A5%87%E0%A4%9F&page=1"
-r = requests.get(url)
-print(r.status_code)
-htmlContent =r.content
-#print(htmlContent)
-soup = BeautifulSoup(htmlContent, 'html.parser')
-#print(soup.prettify)
+# #yo chai just total_page access garana. But, this can be optimize
+# url= "https://bg.annapurnapost.com/api/search?title=%E0%A4%AC%E0%A4%9C%E0%A5%87%E0%A4%9F&page=1"
+# r = requests.get(url)
+# print(r.status_code)
+# htmlContent =r.content
+# #print(htmlContent)
+# soup = BeautifulSoup(htmlContent, 'html.parser')
+# #print(soup.prettify)
 
-title=soup.title
-# print(title)
-data= json.loads(htmlContent)
-total_page=data['data']['totalPage']
+# title=soup.title
+# # print(title)
+# data= json.loads(htmlContent)
+# total_page=data['data']['totalPage']
+
+total_page=1
 
 # print(original[0])
 # print(data['data']['items'][0])
@@ -33,9 +35,9 @@ total_article=0
 # https://bg.annapurnapost.com/api/search?title=%E0%A4%AC%E0%A4%9C%E0%A5%87%E0%A4%9F&page=8
 original=[] #empty list for storing all articles:
 
-def scraping(page_count, total_article):
+def scraping(page_count, total_article,total_page):
     
-  while page_count < total_page:
+  while page_count <= total_page:
     parameters = {'page': page_count}
     # url= 'https://bg.annapurnapost.com/api/search', params=parameters
     r = requests.get('https://bg.annapurnapost.com/api/search?title=%E0%A4%AC%E0%A4%9C%E0%A5%87%E0%A4%9F', params=parameters)
@@ -45,13 +47,16 @@ def scraping(page_count, total_article):
     #if 1st request ma error ayo vane, go to next page:
     if r.status_code != 200:
       page_count =+1
-      scraping(page_count,total_article)
+      scraping(page_count,total_article,total_page)
 
     htmlContent =r.content
     #print(htmlContent)
     soup = BeautifulSoup(htmlContent, 'html.parser')
     data= json.loads(htmlContent)
-    
+
+    if total_page == 1:
+      total_page=data['data']['totalPage']
+
     # print(len(data['data']['items']))
     for x in data['data']['items']:
     
@@ -66,7 +71,7 @@ def scraping(page_count, total_article):
 #end of scraping function:
 
 #calling scraping function:
-scraping(page_count,total_article)
+scraping(page_count,total_article,total_page)
 # print("page count", page_count)
 final_json_object = json.dumps(original)
 
